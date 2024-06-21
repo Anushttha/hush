e5f021af09adcb103c8a3c900f4b360de4797279
anshitsinha <anshit81_soe@jnu.ac.in>
2024-06-21 14:50:20 +0530

    feed fix

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import Post from './Post'
 
+
+
+"use client";
+
+import React, { useEffect, useState } from "react";
 import {
   collection,
   getDocs,
   getFirestore,
   orderBy,
   query,
-} from 'firebase/firestore';
-import { app } from '@/firebase';
+} from "firebase/firestore";
+import { app } from "../firebase"; // Adjust the path as necessary
+import { Post } from "./Post";
 
 
+export default function Feed() {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const db = getFirestore(app);
+      const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+      const querySnapshot = await getDocs(q);
+      let fetchedData = [];
+      querySnapshot.forEach((doc) => {
+        fetchedData.push({ id: doc.id, ...doc.data() });
+      });
+      setData(fetchedData);
+    };
 
-export default async function Feed() {
-  const db = getFirestore(app);
-  const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
-  const querySnapshot = await getDocs(q);
-  let data = [];
-  querySnapshot.forEach((doc) => {
-    data.push({ id: doc.id, ...doc.data() });
-  });
+    fetchData();
+  }, []);
 
-  console.log(data)
   return (
-    <div>
+    <div className=" mx-auto  flex flex-wrap justify-center ">
       {data.map((post) => (
         <Post key={post.id} post={post} id={post.id} />
       ))}
