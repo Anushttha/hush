f8b181ed22eeba1e40354a6792f967fabab0eccf
anshitsinha <anshit81_soe@jnu.ac.in>
2024-06-27 18:35:35 +0530

    post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,6 +15,9 @@ export default function Post({ post, id, comments }) {
         <img src={post?.image} className="rounded-2xl w-[100%]  mr-2" />
       </Link>
       <div className="flex flex-col justify-between">
+
+{/* post */}
+
         <Link href={`/posts/${id}`}>
           <p className="text-gray-800 text-sm my-3 dark:text-light">
             {post?.caption}
